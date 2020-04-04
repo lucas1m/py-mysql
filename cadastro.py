6898cabfd0050e3d9813a0a7358e224396b5cd30
Lucas Matheus <54372996+lucas1m@users.noreply.github.com>
2020-04-03 22:04:06 -0300

    Add files via upload
    Att 2020/04/03

--- cadastro.py
+++ cadastro.py
@@ -3,7 +3,7 @@ import time
 
 
 def _menu():
-    print("Escolha uma opção:\n\n 1)Cadastrar\n 2)Listar cadastros\n 3)Buscar\n 4)Alterar\n 5)Excluir\n")
+    print("\nEscolha uma opção:\n\n 1)Cadastrar\n 2)Buscar\n 3)Alterar\n 4)Excluir\n")
 
 
 def cadastro():
@@ -35,6 +35,90 @@ def cadastro():
 
     time.sleep(5)
 
+def pesquisar():
+    db_connection = mysql.connector.connect(host='localhost', user='lucas', password='147987', database='pythonbd')
+    time.sleep(1)
+    print("\nConexão com o banco realizada...\n")
+
+    pesqID = str(input('Digite o nome a pesquisar: '))
+    time.sleep(2)
+    print("\n")
+
+    cursor = db_connection.cursor()
+    cursor.execute("SELECT * FROM cliente WHERE nome like '"+pesqID+"_%' ")
+
+    resultado = cursor.fetchone() 
+    
+    #metodo fetchall() busca todas as linhas da última instrução executada.
+    #metodo fetchone() é semelhante, porem retorna os dados linha por linha
+
+    for x in resultado:
+        print(x)
+
+
+    cursor.close()
+    db_connection.commit()
+    db_connection.close()
+
+def altera():
+    db_connection = mysql.connector.connect(host='localhost', user='lucas', password='147987', database='pythonbd')
+    time.sleep(1)
+    print("\nConexão com o banco realizada...\n\n")
+
+    alterNOME = str(input('Qual registro será alterado?\nDigite o nome: '))
+
+    cursor = db_connection.cursor()
+    cursor.execute("SELECT * FROM cliente WHERE nome like '"+alterNOME+"_%' ")
+
+    resultado = cursor.fetchone() 
+    
+    #metodo fetchall() busca todas as linhas da última instrução executada.
+    #metodo fetchone() é semelhante, porem retorna os dados linha por linha
+
+    for x in resultado:
+        print(x)
+
+    print("\nRegistro(s) encontrado(s)!\nDigite 1 para confirmar ou 2 para cancelar\n")
+    escolha = int(input(': '))
+    
+    if escolha == 1:
+        print("Ok! Digite o ID desejado\n")
+        id_enc = str(input(': '))
+
+        print(f'Ok! id {id_enc} selecionado\n Agora, o que deseja alterar?\nMENU\n')
+        print('1)Nome\n2)Idade\n3)CPF\n4)Data de nascimento\n\n')
+        
+        escolha2 = int(input('Opcao selecionada: '))
+        if escolha2 == 1:
+            novo_nome = str(input('Digite o novo nome: '))
+            cursor = db_connection.cursor()
+            cursor.execute("Update cliente set nome = '"+novo_nome+"' where id = '"+id_enc+"_%' ")
+            time.sleep(3)
+            print("Cliente atualizado! Voltando ao menu!\n...")
+            time.sleep(5)
+
+            cursor.close()
+            db_connection.commit()
+            db_connection.close()
+            
+            main()
+
+        elif escolha2 == 2:
+            nova_idade = str(input('Digite a idade: '))
+            cursor = db_connection.cursor()
+            cursor.execute("Update cliente set idade = '"+nova_idade+"' where id = '"+id_enc+"_%' ")
+            time.sleep(3)
+            print("Cliente atualizado! Voltando ao menu!\n...")
+            time.sleep(5)
+
+            cursor.close()
+            db_connection.commit()
+            db_connection.close()
+            
+            main()
+        else:
+            print("Continue assim!")
+
 
 def main():
     print("\n")
@@ -44,6 +128,10 @@ def main():
         opcao = int(input('Opção: '))
         if opcao == 1:
             cadastro()
+        elif opcao == 2:
+            pesquisar()
+        elif opcao == 3:
+            altera()
         else:
             print('Opção ainda não disponível')
 
